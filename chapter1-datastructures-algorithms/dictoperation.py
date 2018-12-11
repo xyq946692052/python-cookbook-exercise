99295844ca3f633d69cb902d44802b87a3b3c66c
xyq946692052 <xyq2312@sina.com>
2018-12-11 19:03:34 +0800

    add the relate knowledge of dict opertion

--- chapter1-datastructures-algorithms/dictoperation.py
+++ chapter1-datastructures-algorithms/dictoperation.py
@@ -14,3 +14,25 @@ sort_price = sorted(zip(prices.values(), prices.keys()))
 print('The price of phone: ', prices)
 print('min_price: ',min_price, '\nmax_price', max_price)
 print('sort of price: ', sort_price)
+
+print('*'*50)
+
+a = {'x':1, 'y':2, 'z':3}
+b = {'w':10, 'x':11, 'y':2}
+print(a,'\n',b)
+#find the same keys
+dt = a.keys() & b.keys()
+print('a and b has the same key: ',dt)
+
+#find the key in a not in b
+dt = a.keys() - b.keys()
+print('{} in a not in b'.format(dt))
+
+#find the (key, value) pars in common
+dt = a.items() & b.items()
+print('a and b has the same (key,value): ',dt)
+
+
+#if you want to remove the item in dict, do like this
+c = {key: a[key] for key in a.keys() - {'z','y'}}
+print(c)
