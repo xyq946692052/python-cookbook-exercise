aa13e8b23d80f3f5c83088fc766b29a163018559
xyq946692052 <xyq2312@sina.com>
2019-01-11 02:06:13 +0800

    add file

---
+++ chapter11-network-web/deltest.py
@@ -0,0 +1,44 @@
+import time
+from resty import PathDispatcher
+from wsgiref.simple_server import make_server
+
+_hello_resp = '''\
+<html>
+    <head>
+        <title>Hello {title}</title>
+    </head>
+    <body>
+        <h1>Hello {name}!</h1>
+    </body>
+</html>'''
+
+def hello_world(environ, start_response):
+    start_response('200 OK', [('Content-type', 'text/html')])
+    params = environ['params']
+    resp = _hello_resp.format(name=params.get('name'))
+    yield resp.encode('utf-8')
+
+_localtime_resp = '''\
+<?xml version="1.0"?>
+<time>
+    <year>{t.tm_year}</year>
+    <month>{t.tm_mon}</month>
+    <day>{t.tm_mday}</day>
+    <hour>{t.tm_hour}</hour>
+    <minute>{t.tm_min}</minute>
+    <second>{t.tm_sec}</second>
+</time'''
+
+def localtime(environ, start_response):
+    start_response('200 OK', [('Content-type', 'application/xml')])
+    resp = _localtime_resp.format(t=time.localtime())
+    yield resp.encode('utf-8')
+
+if __name__ == '__main__':
+    dispatcher = PathDispatcher()
+    dispatcher.register('GET', '/hello', hello_world)
+    dispatcher.register('GET', '/localtime', localtime)
+ 
+    httpd = make_server('', 8008, dispatcher)
+    print('Serving on port 8008...')
+    httpd.serve_forever()
